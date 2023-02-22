74ad6519820dbcf07e79a5a8c7595333f0c6dcf6
Bruno Santana <brunosantanadev@gmail.com>
2023-02-22 07:29:10 -0300

    AT2 - 22-02-2023 - curso JS B7

--- Cursos_B7Web/javascript/BlogFake_fetchApi/script.js
+++ Cursos_B7Web/javascript/BlogFake_fetchApi/script.js
@@ -1,14 +1,20 @@
 // https://jsonplaceholder.typicode.com/posts
-
-async function loadingPosts(){
   const elPost = document.querySelector(".posts")
-  elPost.innerHTML = "Carregando..."
 
+async function puxarDados(){
   const response = await fetch("https://jsonplaceholder.typicode.com/posts")
   const json = await response.json()
 
   if(json.length > 0){
-    elPost.innerHTML = "";
+      loadingPosts(json)
+  }else{
+      elPost.innerHTML = "Não ha nenhum post ou a importação dos dados falhou..."
+    }
+}
+puxarDados()
+
+function loadingPosts(json){
+   elPost.innerHTML = "";
   
     for(i of json){
       const post = `<h2>${i.title}</h2> <div>${i.body}</div>`;
@@ -16,8 +22,4 @@ async function loadingPosts(){
       div.innerHTML = post;
       elPost.append(div)
     }
-  }
-
 }
-
-loadingPosts()
