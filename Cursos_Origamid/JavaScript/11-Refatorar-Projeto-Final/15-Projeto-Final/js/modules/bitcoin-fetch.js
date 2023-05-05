d0cdb741d01d4f7da4411eccb2315212f65968ac
Bruno Santana <brunosantanadev@gmail.com>
2023-05-05 14:47:56 -0300

    AT2 - 05-05-2023 - Curso Jc Origamid

--- Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/modules/bitcoin-fetch.js
+++ Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/modules/bitcoin-fetch.js
@@ -1,8 +1,8 @@
-export default function initiBiticoinFetch() {
-  fetch("https://blockchain.info/ticker")
+export default function fetchBiticoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector(".btc-preco");
+      const btcPreco = document.querySelector(target);
       const calculo = (1000 / bitcoin.BRL.sell).toFixed(4);
       btcPreco.innerText = calculo;
     })
--- Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/script.js
+++ Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/script.js
@@ -1,16 +1,15 @@
+document.documentElement.className += ' js';
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/init-acoordion.js';
 import TabNav from './modules/init-tab-nav.js';
 import Modal from './modules/modal.js';
 import ToolTip from './modules/tooltip.js';
 import fetchAnimais from './modules/animais_fetch.js';
+import fetchBiticoin from './modules/bitcoin-fetch.js';
 import initDropDownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initDataObject from './modules/date_object.js';
-import initBitcoiFetch from './modules/bitcoin-fetch.js';
 import initAnimaScroll from './modules/init-anima-scroll.js';
-
-document.documentElement.className += ' js';
 
 const scrollSuave = new ScrollSuave("[data-scroll='suave'] a[href^='#']");
 scrollSuave.init();
@@ -30,8 +29,9 @@ tooltip.init();
 
 fetchAnimais("/animaisApi.json", ".numeros-grid");
 
+fetchBiticoin("https://blockchain.info/ticker",".btc-preco");
+
 initDropDownMenu();
 initMenuMobile();
 initDataObject();
-initBitcoiFetch();
 initAnimaScroll();
