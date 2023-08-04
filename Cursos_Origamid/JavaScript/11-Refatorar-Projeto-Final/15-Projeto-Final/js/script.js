c5c95c72101c87e1437d20105c131066052c7677
Bruno Santana <brunosantanadev@gmail.com>
2023-08-04 12:55:52 -0300

    Enviando Contra Cheque

--- Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/script.js
+++ Cursos_Origamid/JavaScript/11-Refatorar-Projeto-Final/15-Projeto-Final/js/script.js
@@ -20,8 +20,7 @@ accordion.init();
 const tabNav = new TabNav("[data-tab='menu'] li","[data-tab='content'] section");
 tabNav.init();
 
-const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']", 
-"[data-modal='container']");
+const modal = new Modal("[data-modal='abrir']", "[data-modal='fechar']","[data-modal='container']");
 modal.init();
 
 const tooltip = new ToolTip("[data-tooltip]");
